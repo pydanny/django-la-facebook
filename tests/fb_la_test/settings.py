1fe47c0d9fc880a7000cd8c9cea374bacec0c109
Jacob Burch <jacobburch@gmail.com>
2011-02-05 13:12:51 -0800

    Added la_facebook settings

--- tests/fb_la_test/settings.py
+++ tests/fb_la_test/settings.py
@@ -89,8 +89,28 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'fb_la_test.connect'
+    'fb_la_test.connect',
+    'la_facebook',
 )
 
-
 FACEBOOK_APP_ID = '124397597633470'
+FACEBOOK_API_KEY = '0d6acba060823bac2f93708d98d7e74a'
+FACEBOOK_APP_SECRET = 'cdd60917e6a30548b933ba91c48289bc'
+
+OAUTH_ACCESS_SETTINGS = {
+    "facebook": {
+        "keys": {
+            "KEY": FACEBOOK_APP_ID,
+            "SECRET": FACEBOOK_APP_SECRET,
+        },
+        "endpoints": {
+            # OAuth 2.0 does not need a request token
+            "access_token": "https://graph.facebook.com/oauth/access_token",
+            "authorize": "https://graph.facebook.com/oauth/authorize",
+            # Will need to redo the following
+            "callback": "la_facebook.oauth.facebook_callback",
+            # Probably too much power here - just need to have authentication
+            "provider_scope": ["publish_stream"],
+        }
+    }
+}
