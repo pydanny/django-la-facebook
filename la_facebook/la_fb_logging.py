500b59e7026091bb1e34db3f19495e4b77b4a244
Preston Holmes <preston@ptone.com>
2011-02-05 14:56:24 -0800

    defined basic logger

--- la_facebook/la_fb_logging.py
+++ la_facebook/la_fb_logging.py
@@ -1,2 +1,12 @@
 import logging
+import sys
+
+logger = logging.getLogger("la_fb")
+# @@ TODO: get log level from settings
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+
+logger.addHandler(handler)
+
 
