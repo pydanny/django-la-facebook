8c0e1a8badd6212a12e8a5e19321a2db5e76369d
Alexandros Bantis <ambantis@gmail.com>
2011-02-05 18:36:21 -0800

    change string to raw

--- tests/fb_la_test/connect/tests/test_connect.py
+++ tests/fb_la_test/connect/tests/test_connect.py
@@ -35,7 +35,7 @@ class TestConnection(LaFacebookTestCase):
         print content      
         
         form_data = dict(
-            charset_test=unicode("&euro;,&acute;,\xe2,\xc2,\xd0,\xd0", "utf-8"),
+            charset_test=unicode(r"&euro;,&acute;,\xe2,\xc2,\xd0,\xd0", "utf-8"),
             lsd="IuLli",
             next="http://www.facebook.com/connect/uiserver.php?method=permissions.request&amp;app_id=124397597633470&amp;display=page&amp;redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fafter&amp;response_type=code&amp;fbconnect=1&amp;perms=email%2Cread_stream&amp;from_login=1",
             api_key="124397597633470",
