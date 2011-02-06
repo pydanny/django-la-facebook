cf58e9447e0ab2ea1fb0f6965d471532aad613cb
Jacob Burch <jacobburch@gmail.com>
2011-02-05 18:19:56 -0800

    Logging Todo Done

--- la_facebook/la_fb_logging.py
+++ la_facebook/la_fb_logging.py
@@ -5,7 +5,6 @@ from django.conf import settings
 LOG_FILE = getattr(settings, 'FACEBOOK_LOG_FILE', '/tmp/fb_logging.log')
 
 logger = logging.getLogger("la_fb")
-# @@ TODO: get log level from settings
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
