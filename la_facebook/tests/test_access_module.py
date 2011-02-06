beb6f77632516201e8283f5272f9d0dcc25aab61
Preston Holmes <preston@ptone.com>
2011-02-05 19:10:32 -0800

    first OAuthAccess tests

--- la_facebook/tests/test_access_module.py
+++ la_facebook/tests/test_access_module.py
@@ -1,19 +1,28 @@
 from django.conf import settings
 from django.test import TestCase
-
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 from la_facebook.access import OAuthAccess
+from la_facebook.utils.loader import load_path_attr
 
 class PropertyTests(TestCase):
-    
+
     def test_key_in_settings(self):
         # test if there is a key
-        oath = OAuthAccess()
-        self.assertEquals(oath.key, "124397597633470")
-        #self.assertRaises(NameError, runt_this)
-        
+        oauth = OAuthAccess()
+        self.assertEquals(oauth.key, "124397597633470")
+
     def test_callback_url(self):
-        
-        pass
-        
+        oauth = OAuthAccess()
+        callback_url = oauth.callback_url
+        current_site = Site.objects.get(pk=settings.SITE_ID)
+        base_url = "http://%s" % current_site.domain
+        reversed_url = reverse("la_facebook_callback")
+        expected_url = "%s%s" % (base_url, reversed_url)
+        self.assertEquals(callback_url,expected_url)
+
     def test_callback(self):
-        pass
+        oauth = OAuthAccess()
+        callback_endpoint = oauth.callback
+        expected_callback_endpoint = load_path_attr(oauth._obtain_setting("endpoints", "callback"))
+        self.assertEquals(callback_endpoint,expected_callback_endpoint)
